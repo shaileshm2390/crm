09f158a107e392635435ab768c70107c897930c2
shaileshm2390 <40160246+shaileshm2390@users.noreply.github.com>
2018-06-14 18:56:26 +0530

    Update sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -12,8 +12,9 @@ var db = {};
 winston.info('Initializing Sequelize...');
 
 // create your instance of sequelize
-var onHeroku = !!process.env.DYNO;
-//var onHeroku = false;
+//var onHeroku = !!process.env.DYNO;
+var onHeroku = false;
+console.log('Checking if running on Heroku: ',onHeroku);
 winston.info('Checking if running on Heroku: ',onHeroku);
 
 var sequelize =  onHeroku ?

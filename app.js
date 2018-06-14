261ba6aed45062710b0a4189f5c5cc199d5513d7
shaileshm2390 <40160246+shaileshm2390@users.noreply.github.com>
2018-06-14 13:34:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use(session({
 require('./config/express')(app, passport);
 
 //Start the app by listening on <port>
-app.listen(3000);
+app.listen(process.env.PORT || config.PORT);
 winston.info('Express app started on port ' + config.PORT);
 
 //expose app

30010a1b671700dc5616c8322f9f38f4f93439ee
Shailesh Mishra <shaileshm@imorsetech.com>
2018-06-29 14:22:24 +0530

    Modified some changes regarding designs

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,10 +10,13 @@ $(function() {
 
 
     var i = function() {
-        (window.innerWidth > 0 ? window.innerWidth : this.screen.width) < 1170 ? ($("body").addClass("mini-sidebar"),
+        //(window.innerWidth > 0 ? window.innerWidth : this.screen.width) < 1170 ? ($("body").addClass("mini-sidebar"),
+        //    $(".navbar-brand span").hide(), $(".scroll-sidebar, .slimScrollDiv").css("overflow-x", "visible").parent().css("overflow", "visible"),
+        //    $(".sidebartoggler i").addClass("ti-menu")) : ($("body").removeClass("mini-sidebar"),
+        //    $(".navbar-brand span").show());
+        $("body").addClass("mini-sidebar"),
             $(".navbar-brand span").hide(), $(".scroll-sidebar, .slimScrollDiv").css("overflow-x", "visible").parent().css("overflow", "visible"),
-            $(".sidebartoggler i").addClass("ti-menu")) : ($("body").removeClass("mini-sidebar"),
-            $(".navbar-brand span").show());
+            $(".sidebartoggler i").addClass("ti-menu")
         var i = (window.innerHeight > 0 ? window.innerHeight : this.screen.height) - 1;
         (i -= 70) < 1 && (i = 1), i > 70 && $(".page-wrapper").css("min-height", i + "px");
     };
